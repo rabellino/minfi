267cdf74811e13d1274b4cec8a624e7ee399d6bf
Mike Rabellino <mdrabell@gmail.com>
2025-03-18 14:09:06 -0600

    adding some more styling

--- client/src/components/layouts/Home.jsx
+++ client/src/components/layouts/Home.jsx
@@ -7,7 +7,19 @@ import HealthCheck from "../../components/displayElements/appComponents/HealthCh
 
 function Home() {
   return (
-    <Flex flexDir="column" w="50%">
+    <Flex
+      flexDir="column"
+      w="50%" 
+      alignItems="center" // Centers content horizontally
+      justifyContent="center" // Centers content vertically
+      height="100vh" // Takes full viewport height
+      margin="auto" // Ensures horizontal centering
+      gap="1.5rem" // Adds space between child elements
+    >
+      <Text textAlign="left" fontSize="lg" mb="1rem" color="gray.700">
+        This application provides a health check for the backend services. 
+        Click the button below to check the API status.
+      </Text>
       <HealthCheck />
     </Flex>
  );
